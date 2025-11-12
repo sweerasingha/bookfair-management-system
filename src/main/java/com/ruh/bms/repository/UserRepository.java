02a2f21c54d663339b4ee09c25076f528962dea0
sweerasingha <sachiprojects@gmail.com>
2025-11-12 18:19:00 +0530

    updated user repository

--- src/main/java/com/ruh/bms/repository/UserRepository.java
+++ src/main/java/com/ruh/bms/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.ruh.bms.repository;public interface UserRepository {
+package com.ruh.bms.repository;
+
+import com.ruh.bms.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
