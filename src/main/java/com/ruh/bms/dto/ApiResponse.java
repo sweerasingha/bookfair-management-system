1bd6ea2a7bb1d901a2671231623c3f24dc2e7729
sweerasingha <sachiprojects@gmail.com>
2025-11-12 18:24:27 +0530

    implemented the api response file

--- src/main/java/com/ruh/bms/dto/ApiResponse.java
+++ src/main/java/com/ruh/bms/dto/ApiResponse.java
@@ -1,2 +1,19 @@
-package com.ruh.bms.dto;public class ApiResponse {
+package com.ruh.bms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+
+    private boolean success;
+    private String message;
+    private Object data;
+    private LocalDateTime timestamp;
+
 }
