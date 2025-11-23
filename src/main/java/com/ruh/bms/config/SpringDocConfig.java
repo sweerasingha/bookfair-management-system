93455a8e1a794e602e1b895adffe4b8f2a266ac4
Shenith Bandara <139686889+Shenith404@users.noreply.github.com>
2025-11-24 03:15:27 +0530

    add swagger api for the project

---
+++ src/main/java/com/ruh/bms/config/SpringDocConfig.java
@@ -0,0 +1,33 @@
+package com.ruh.bms.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Essay-Grading API")
+                        .description("API")
+                        .version("1.0"))
+         .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
